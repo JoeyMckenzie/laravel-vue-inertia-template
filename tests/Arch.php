6c20927727bd5ce479f11b7c8a54321bb9679316
Joey McKenzie <joey.mckenzie27@gmail.com>
2024-05-25 10:31:16 -0700

    chore: add arch tests

---
+++ tests/Arch.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests;
+
+test('All source files are strictly typed')
+    ->expect('App\\')
+    ->toUseStrictTypes();
+
+test('All tests files are strictly typed')
+    ->expect('Tests\\')
+    ->toUseStrictTypes();
